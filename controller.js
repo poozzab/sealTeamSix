31fd5be16e9c12d4e7def950999929c27340f55b
Daniel Barker <poozzab@gmail.com>
2022-09-30 16:25:10 -0700

    Imports and Server Validation
    Imported Dependencies for Express, JQuery, Pug
    Also supplied some routes for extremely basic validation

--- controller.js
+++ controller.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post('/bungieUser', (req, res) => {
+    var request = $(req)
+    var username = request['username'];
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    
 })
